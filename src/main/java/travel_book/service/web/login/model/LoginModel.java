97350fc859b97944ca0bde003b474e865f6f47c7
Jucy92 <cyju92@gmail.com>
2024-08-28 23:17:44 +0900

    모델 수정

--- src/main/java/travel_book/service/web/login/model/LoginModel.java
+++ src/main/java/travel_book/service/web/login/model/LoginModel.java
@@ -8,9 +8,11 @@ import lombok.Data;
 @Data
 public class LoginModel {
 
-    @Email
-    @NotBlank(message = "메일주소를 입력해 주세요.")
-    private String mail;
+//    @Email
+//    @NotBlank(message = "메일주소를 입력해 주세요.")
+//    private String mail;
+    @NotBlank(message = "아이디를 입력해 주세요.")
+    private String userId;
     @NotEmpty(message = "비밀번호를 입력해 주세요.")
     private String password;
     //private String phone;     // 로그인 페이지에서는 두개만 입력 받으면 되는거니깐..?
